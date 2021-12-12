992fd23189caef86e2109233d36ba1b994af8216
Alex Collier <alex@coalminestudios.ca>
2021-12-12 16:52:33 -0500

    chore: moves page styles to `styles/pages`

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,5 @@
-import type { NextPage } from 'next';
 import ContactForm from '@/components/ContactForm';
+import type { NextPage } from 'next';
 import Head from 'next/head';
 
 const Contact: NextPage = () => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Canary from '@/components/Canary';
-import styles from '@/styles/Home.module.css';
+import styles from '@/styles/pages/Home.module.css';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
