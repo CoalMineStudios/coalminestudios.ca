d489e8a5c767c3023a017123d68366cfb49dc713
Alex Collier <alex@coalminestudios.ca>
2021-12-12 20:40:48 -0500

    chore: uses alias

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 import 'sanitize.css';
 import 'sanitize.css/forms.css';
-import '../styles/globals.scss';
+import '@/styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
